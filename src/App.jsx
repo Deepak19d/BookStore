79fbaf5b396154b44c4b105c80be26417142a7c1
Deepak kumar <deepak19122002@gmail.com>
2024-07-22 10:55:43 +0530

    Banner build

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
     <Navbar />
+    <Banner />
+    <Footer />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Navbar() {
+  const [sticky, setSticky]=useState(false)
+  useEffect(()=>{
+    const handleScroll=()=>{
+      if(window.scrollY>0){
+        setSticky(true)
+      }
+      else{
+        setSticky(false)
+      }
+    }
+    window.addEventListener('scroll',handleScroll)
+    return ()=>window.removeEventListener('scroll',handleScroll)
+  },[])
     const navItems=(<>
      <li>
             <a>Home</a>
@@ -18,8 +31,10 @@ function Navbar() {
   return (
    <>
    <div>
-    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'></div>
-   <div className="navbar bg-base-100">
+    <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${
+      sticky?"sticky-navbar shadow-md bg-base-200 duration-200 transition-all ease-in-out":""
+    }`}>
+   <div className="navbar ">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -41,7 +56,7 @@ function Navbar() {
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
       {navItems}</ul>
     </div>
-    <a className=" text-2xl font-bold cursor-pointer">BookStore</a>
+    <a className=" text-2xl font-bold cursor-pointer">Book Shelf</a>
   </div>
   <div className="navbar-end space-x-3">
   <div className="navbar-center hidden lg:flex">
@@ -89,6 +104,7 @@ function Navbar() {
   </div>
 </div>
 </div>
+   </div>
    </div>
    </>
   )
