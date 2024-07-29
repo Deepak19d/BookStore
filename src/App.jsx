5d8d59ed941711bb9cc6fcc85dafd5ae64850e04
Deepak kumar <deepak19122002@gmail.com>
2024-07-29 23:01:55 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Banner from './components/Banner';
-import Footer from './components/Footer';
-import FreeBook from './components/FreeBook';
+import React from "react";
+import Home from "./components/home/Home";
+import Course from "./components/Course";
+import { Route, Routes } from "react-router-dom"
 function App() {
   return (
     <>
-    <Navbar />
-    <Banner />
-    <FreeBook />
-    <Footer />
+      {/* <Home />
+      <Course /> */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/course" element={<Course/>}/>
+      </Routes>
     </>
   );
 }
