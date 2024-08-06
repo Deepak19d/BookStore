3d5f8f180a3753bc8b8c4d0da8dae75021f76347
Deepak kumar <deepak19122002@gmail.com>
2024-08-06 23:42:34 +0530

    commit

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -9,6 +9,15 @@ function Course() {
             Discover a Universe of Books at Your{" "}
             <span className="text-blue-500">Fingertips.</span>
           </h1>
+          <p className="mt-12">
+            {" "}
+            Our courses are like those viral cat videos – once you start, you
+            can't stop! 😂 Whether you're a coding newbie or a pro looking to
+            upgrade your skills, our book's got all the hacks you need.
+          </p>
+          <button className=" mt-6 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 duration-300">
+            Back
+          </button>
         </div>
       </div>
     </>
