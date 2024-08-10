80eda97145a14a21483a9a322a536914b98ae068
Deepak kumar <deepak19122002@gmail.com>
2024-08-10 23:00:49 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Courses from "./courses/Courses";
 function App() {
   return (
     <>
-      {/* <Home />
-      <Course /> */}
+      <div className="dark:bg-slate-900 dark:text-white">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course" element={<Courses />} />
       </Routes>
+      </div>
     </>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,7 @@ function Cards({ item }) {
   return (
     <>
       <div className="mt-4 my-3 p-3">
-        <div className="card bg-base-100 w-92 shadow-xl hover:scale-105 duration-200">
+        <div className="card bg-base-100 w-92 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
           <figure>
             <img src={item.image} alt="BookCover" />
           </figure>
